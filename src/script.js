8e7613203e4c5c5de9136c732a5f2ef072da67f2
EnnaLeuark <Akrauel@posteo.de>
2021-06-21 22:11:55 +0200

    css updates, additional data

--- src/script.js
+++ src/script.js
@@ -35,18 +35,23 @@ if (min < 10) {
 }
 
 let currentTime = document.querySelector("#currentTime");
-currentTime.innerHTML = ` ${hour}:${min}`;
+currentTime.innerHTML = ` ${hour}:${min} (CET)`;
 
 // City Search & current Temperature
 let apiKey = "689f4f9c85431deeb4c8640074154109";
 let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 //Beispiel URL:"https://api.openweathermap.org/data/2.5/weather?q=Sydney"
 
+// to access Current and forecast weather data:
+//let apiURL2="https://api.openweathermap.org/data/2.5/onecall?lat=52.3989&lon=13.0657&exclude=minutely,hourly";
+
 function showWeather(response) {
   //Temperature
+  console.log(response);
   let temperature = Math.round(response.data.main.temp);
   let tempMin = Math.round(response.data.main.temp_min);
   let tempMax = Math.round(response.data.main.temp_max);
+
   let temperatureElement = document.querySelector("#degreeTemp");
   temperatureElement.innerHTML = temperature;
   let tempMinElement = document.querySelector("#currentTemp_min");
@@ -54,14 +59,29 @@ function showWeather(response) {
   let tempMaxElement = document.querySelector("#currentTemp_max");
   tempMaxElement.innerHTML = `${tempMax}°C`;
   let location = document.querySelector(".cityName");
-  location.innerHTML = response.data.name;
+  location.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  let IconElement = document.querySelector("#currentIcon");
+  IconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  IconElement.setAttribute("alt", response.data.weather[0].description);
 
   //description
   let description = response.data.weather[0].description;
   let descriptionElement = document.querySelector("#currentDescription");
   descriptionElement.innerHTML = description;
 
-  //rain
+  //feels like
+  let feelsLike = Math.round(response.data.main.feels_like);
+
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  feelsLikeElement.innerHTML = feelsLike;
+
+  //Humidity
+  let Humidity = response.data.main.humidity;
+  let HumidityElement = document.querySelector("#humidity");
+  HumidityElement.innerHTML = ` ${Humidity} `;
 
   //windspeed
   let windspeed = Math.round(response.data.wind.speed);
@@ -94,7 +114,7 @@ function showPositionWeather(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "0acaa1de783699a7c805c0e5bad91ab8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q&lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}`).then(showWeather);
 }
 
@@ -108,14 +128,18 @@ LocationButton.addEventListener("click", getCurrentPosition);
 
 //Celsius to Fahrenheit Conversion
 //°C = (°F - 32) * 5/9 (von Fahrenheit in Celsius)
-/* function convertToFahrenheit(event) {
+/* function displayFahrenheitTemp(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#degreeTemp");
-  temperatureElement.innerHTML = "48";
+  //temperatureElement.innerHTML = "48";
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
 }
 
 let temperature = document.querySelector("#FahrenheitLink");
-temperature.addEventListener("click", convertToFahrenheit);
+temperature.addEventListener("click", displayFahrenheitTemp);
 
 // Fahrenheit to Celsius
 //°F = °C * 1,8 + 32 (von Celsius nach Fahrenheit)
